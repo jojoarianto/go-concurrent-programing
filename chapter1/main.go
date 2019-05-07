1438378965d95ff22fd75b34a5fae9fd3c52a416
Joko Irianto <irianto94@outlook.com>
2019-05-07 17:41:24 +0700

    basic go routine

---
+++ chapter1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Chapter 1")
+
+	go function1("goroutine2", 5) // run with go routine
+	go function1("goroutine3", 5) // run with go routine
+
+	function1("main1", 15)
+
+}
+
+func function1(identifier string, n int) {
+
+	for index := 0; index < n; index++ {
+		time.Sleep(time.Second * 1)
+		fmt.Println("Goroutine ", identifier, " caunt on", index)
+	}
+
+	return
+}
